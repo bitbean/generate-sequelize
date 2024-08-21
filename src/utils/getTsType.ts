7abc6c5367be25cd0b63f8a129d77d98284bd7b6
yankie drebin <yankie@bitbean.com>
2024-08-21 14:09:22 -0400

    fix range type

--- src/utils/getTsType.ts
+++ src/utils/getTsType.ts
@@ -1,40 +1,48 @@
-import getEnumValues from './getEnumValues';
+import getEnumValues from "./getEnumValues";
 
-export default function getTsType(type: string, special?: string[], elementType?: string): string {
+export default function getTsType(
+  type: string,
+  special?: string[],
+  elementType?: string,
+): string {
   const fieldType = type.toLowerCase();
 
   let jsType: string;
 
   if (isArray(fieldType)) {
-    if (/(daterange)/.test(fieldType)) {
-      jsType = '[Date, Date]';
+    if (/(range)/.test(fieldType)) {
+      let rangeType = "number";
+      if (["daterange", "tsrange", "tstzrange"].includes(fieldType)) {
+        rangeType = "Date | string";
+      }
+      jsType = `[lower: { value: ${rangeType}, inclusive: boolean }, higher: { value: ${rangeType}, inclusive: boolean }]`;
     } else {
-      const eltype = elementType ? getTsType(elementType) : 'unknown';
-      jsType = eltype + '[]';
+      const eltype = elementType ? getTsType(elementType) : "unknown";
+      jsType = eltype + "[]";
     }
   } else if (isNumber(fieldType)) {
-    jsType = 'number';
+    jsType = "number";
   } else if (isBoolean(fieldType)) {
-    jsType = 'boolean';
+    jsType = "boolean";
   } else if (isDate(fieldType)) {
-    jsType = 'Date';
+    jsType = "Date";
   } else if (isString(fieldType)) {
-    jsType = 'string';
+    jsType = "string";
   } else if (isEnum(fieldType)) {
     const values = getEnumValues(type, special);
-    jsType = values.join(' | ');
+    jsType = values.join(" | ");
   } else if (isJSON(fieldType)) {
-    jsType = 'object';
+    jsType = "object";
   } else {
     console.log(`Missing TypeScript type: ${fieldType}`);
-    jsType = 'unknown';
+    jsType = "unknown";
   }
   return jsType;
 }
 
 function isNumber(fieldType: string): boolean {
   return /^(smallint|mediumint|tinyint|int|bigint|float|money|smallmoney|double|decimal|numeric|real|oid|smallserial|serial|bigserial)/.test(
-    fieldType
+    fieldType,
   );
 }
 
@@ -48,7 +56,7 @@ function isDate(fieldType: string): boolean {
 
 function isString(fieldType: string): boolean {
   return /^(char|nchar|string|varying|varchar|nvarchar|text|longtext|mediumtext|tinytext|ntext|uuid|uniqueidentifier|date|time|inet|cidr|macaddr|tsvector|tsquery|hstore|point|line|lseg|box|path|polygon|circle|interval|tsvector|hstore)/.test(
-    fieldType
+    fieldType,
   );
 }
 
